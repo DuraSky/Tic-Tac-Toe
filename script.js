1a7a6261a72ecc12ff342c68f7571c1b6eb9c200
Lukas Broskovic <lukas.broskovic@gmail.com>
2023-11-27 17:01:56 +0100

    marker switching works, temporarily in a while loop. have to figure out winning coniditons next

--- script.js
+++ script.js
@@ -5,38 +5,58 @@ const gameBoard = (function () {
     for (let i = 0; i < rowsAndColums; i++) {
         board[i] = [];
         for (let j = 0; j < rowsAndColums; j++) {
-          board[i].push("X");
-        }
-    }
+          board[i].push(" ");
+        };
+    };
+
     return board;
 })();
 
-const getCoordinates = function(){
-    const choiceRow = prompt("give row 0-2");
-    const choiceColumn = prompt("give column 0-2");
 
-    if(choiceColumn > 2 || choiceRow > 2){
-        alert("wrong number");
+
+
+
+const gameRules = (function (){
+    let playerFlag = true;
+    let gameCounter = 0;
+
+    while(gameCounter < 4){    
+    const getCoordinates = () =>{
+        const choiceRow = prompt("give row 0-2");
+        const choiceColumn = prompt("give column 0-2");
+    
+        if(choiceColumn > 2 || choiceRow > 2){
+            alert("wrong number");
+        };
+
+     return{choiceColumn, choiceRow};
+    };
+
+    let coordinates = getCoordinates();
+  
+    const playTurn = () =>{
+        const myBoard = gameBoard;
+        const {choiceColumn, choiceRow} = coordinates;
+        
+        let choiceColumnInt = parseInt(choiceColumn);
+        let choiceRowInt = parseInt(choiceRow);
+
+        if(playerFlag === true){
+            myBoard[choiceRowInt][choiceColumnInt] = "O";
+            playerFlag = false;
+        }else if(playerFlag === false){
+            myBoard[choiceRowInt][choiceColumnInt] = "X";
+            playerFlag = true;
+        }
+        console.log(myBoard);
+     gameCounter++;
     }
 
-    return{choiceColumn, choiceRow};
+    playTurn();
 };
 
-const playerRound = function(gameBoard,choiceColumn,choiceRow){
-     return (gameBoard[choiceRow][choiceColumn] = "O")
- };
+    //return{getCoordinates, playTurn};
+})();
 
 
-const playOneGame = function(){
-    const coordinates = getCoordinates();
-    const { choiceColumn, choiceRow } = coordinates;
-    playerRound(gameBoard, choiceColumn, choiceRow);
-    console.log(gameBoard);
-}
 
-playOneGame();
-playOneGame();
-playOneGame();
-playOneGame();
-playOneGame();
-playOneGame();
