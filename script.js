d2139be233f3f4964fc60a1538690a0b5d1022c1
Lukas Broskovic <lukas.broskovic@gmail.com>
2023-11-29 11:22:29 +0100

    game working in console, all the features should now works except for tie

--- script.js
+++ script.js
@@ -8,55 +8,106 @@ const gameBoard = (function () {
           board[i].push(" ");
         };
     };
-
     return board;
 })();
 
-
-
-
-
 const gameRules = (function (){
     let playerFlag = true;
-    let gameCounter = 0;
+    let isGameWon = false;
 
-    while(gameCounter < 4){    
-    const getCoordinates = () =>{
-        const choiceRow = prompt("give row 0-2");
-        const choiceColumn = prompt("give column 0-2");
-    
-        if(choiceColumn > 2 || choiceRow > 2){
-            alert("wrong number");
+    while(isGameWon === false){    
+        const getCoordinates = () =>{
+            let choiceRow, choiceColumn;
+
+           // while (true) {
+                choiceRow = parseInt(prompt("Give row 0-2"));
+                choiceColumn = parseInt(prompt("Give column 0-2"));
+        
+                if (
+                    (choiceRow >= 0 && choiceRow <= 2 && !isNaN(choiceRow)) &&
+                    (choiceColumn >= 0 && choiceColumn <= 2 && !isNaN(choiceColumn))
+                ) {
+                    break; // Break the loop if both row and column values are valid
+                } else {
+                    alert("Invalid input. Please enter a number between 0 and 2.");
+                }
+            }
+        
+            return { choiceColumn, choiceRow };
         };
 
-     return{choiceColumn, choiceRow};
+        let coordinates = getCoordinates();
+
+        const playTurn = () =>{
+            const myBoard = gameBoard;
+            const {choiceColumn, choiceRow} = coordinates;
+
+            if(myBoard[choiceRow][choiceColumn] === "O"){ //check if space is already populated, if so, push either X or O back
+                alert("Already taken");
+                myBoard[choiceRow][choiceColumn] = "O";
+            }else if(myBoard[choiceRow][choiceColumn] ==="X"){
+                alert("Already taken");
+                myBoard[choiceRow][choiceColumn] = "X";
+            }else{
+                if(playerFlag === true){                        //if its not populated, push X or O
+                    myBoard[choiceRow][choiceColumn] = "O";
+                    playerFlag = false;
+                }else if(playerFlag === false){
+                    myBoard[choiceRow][choiceColumn] = "X";
+                    playerFlag = true;
+                }
+        };
+            console.log(...myBoard);
+            return myBoard;
+        };
+        let boardToCheck = playTurn();
+
+        const gameOver = () =>{
+            const {choiceColumn, choiceRow} = coordinates;
+
+            if(boardToCheck[choiceRow][0]==="O" //horizontal win condition for O
+            &&boardToCheck[choiceRow][1]==="O"
+            &&boardToCheck[choiceRow][2]==="O"){
+                alert("O won");
+                return isGameWon = true;
+            }else if(boardToCheck[0][choiceColumn]==="O" // vertical win conditions for O
+            &&boardToCheck[1][choiceColumn]==="O"
+            &&boardToCheck[2][choiceColumn]==="O"){
+                alert("O won");
+                return isGameWon = true;
+            }else if(boardToCheck[1][1]==="O" // diagonal for O
+            &&boardToCheck[0][0]==="O"
+            &&boardToCheck[2][2]==="O"){
+                alert("O won");
+                return isGameWon = true;
+            }else if(boardToCheck[1][1]==="O" //  diagonal for O
+            &&boardToCheck[0][2]==="O"
+            &&boardToCheck[2][0]==="O"){
+                alert("O won");
+                return isGameWon = true;
+            }else if(boardToCheck[choiceRow][0]==="X" 
+            &&boardToCheck[choiceRow][1]==="X"
+            &&boardToCheck[choiceRow][2]==="X"){
+                alert("X won");
+                return isGameWon = true;
+            }else if(boardToCheck[0][choiceColumn]==="X" 
+            &&boardToCheck[1][choiceColumn]==="X"
+            &&boardToCheck[2][choiceColumn]==="X"){
+                alert("X won");
+                return isGameWon = true;
+            }else if(boardToCheck[1][1]==="X" 
+            &&boardToCheck[0][0]==="X"
+            &&boardToCheck[2][2]==="X"){
+                alert("X won");
+                return isGameWon = true;
+            }else if(boardToCheck[1][1]==="X"
+            &&boardToCheck[0][2]==="X"
+            &&boardToCheck[2][0]==="X"){
+                alert("X won");
+                return isGameWon = true;
+            }
+        };
+        gameOver();
+        console.log(isGameWon);
     };
-
-    let coordinates = getCoordinates();
-  
-    const playTurn = () =>{
-        const myBoard = gameBoard;
-        const {choiceColumn, choiceRow} = coordinates;
-        
-        let choiceColumnInt = parseInt(choiceColumn);
-        let choiceRowInt = parseInt(choiceRow);
-
-        if(playerFlag === true){
-            myBoard[choiceRowInt][choiceColumnInt] = "O";
-            playerFlag = false;
-        }else if(playerFlag === false){
-            myBoard[choiceRowInt][choiceColumnInt] = "X";
-            playerFlag = true;
-        }
-        console.log(myBoard);
-     gameCounter++;
-    }
-
-    playTurn();
-};
-
-    //return{getCoordinates, playTurn};
 })();
-
-
-
